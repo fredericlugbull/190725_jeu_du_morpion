25ba93b56c92d900e987559a44a02a45dcf7d2cc
Frédéric <f.lugbull@gmail.com>
2019-07-25 23:37:22 +0200

    yo

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -9,31 +9,138 @@
 class Board < BoardCase
   #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
   #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
-  attr_accessor :BoardCase_array
+  attr_accessor :boardcases_array, :count_turn
 
   def initialize
     #TO DO :
     #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
     #Ces instances sont rangées dans un array/hash qui est l'attr_accessor de la classe
-    @a1 = BoardCase.new("a1" , "  ")
-    @a2 = BoardCase.new("a2" , "  ")
-    @a3 = BoardCase.new("a3" , "  ")
-    @b1 = BoardCase.new("b1" , "  ")
-    @b2 = BoardCase.new("b2" , "  ")
-    @b3 = BoardCase.new("b3" , "  ")
-    @c1 = BoardCase.new("c1" , "  ")
-    @c2 = BoardCase.new("c2" , "  ")
-    @c3 = BoardCase.new("c3" , "  ")
-@array_case= [ @a1 , @a2 , @a3, @b1, @b2, @b3, @c1, @c2, @c3]
+    @boardcase_array = []
+    i = 0
+    9.times do
+      @boardcase_array << BoardCase.new(i)
+      i += 1
+    end
   end
 
   def play_turn
     #TO DO : une méthode qui :
     #1) demande au bon joueur ce qu'il souhaite faire
     #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
-  end
+    case_choice = []
+    boardcase_array.each do |c|
+      if c.value == " "
+      case_choice << c.content
+      end
+    end
+    user_choice = prompt.select("Que voulez-vous faire #{player.name}?", case_choice)   #Créée un user_choice qui va ensuite servir à écraser la valeur existante
+    boardcase_array[user_choice].value = player.player_symbol
+    puts player_symbol
+  end 
+   
+    
 
   def victory?
     #TO DO : une méthode qui vérifie le plateau et indique s'il y a un vainqueur ou match nul
+  end
+end
+
+
+
+CARSANO:
+
+class Board
+  attr_reader :cases, :nb_coups_joues, :aborted
+  
+  def initialize
+    # Ici on initialise un plateau
+    # On crée les boardcases
+    a1=BoardCase.new(1) 
+    a2=BoardCase.new(2)
+    a3=BoardCase.new(3) 
+    b1=BoardCase.new(4) 
+    b2=BoardCase.new(5) 
+    b3=BoardCase.new(6) 
+    c1=BoardCase.new(7) 
+    c2=BoardCase.new(8) 
+    c3=BoardCase.new(9)  
+    @cases = [a1, a2, a3, b1, b2, b3, c1, c2, c3] # On stocke les boardcases
+    @nb_coups_joues = 0
+    @aborted = false
+  end
+
+  def change_value_case(bcase, value) # On applique le changement à la case
+    begin
+      @cases[bcase].change_content(value)
+      add_coup
+    rescue
+      ending_play # Si on joue quitter
+    end
+  end
+
+  def ending_play
+    @aborted = true
+  end
+
+  def add_coup
+    @nb_coups_joues += 1
+  end
+
+  def verif_alignement_points # On vérifie si le joueur gagne
+    if verif_lines or verif_columns or verif_diagos
+      true
+    else
+      false
+    end
+  end
+
+  def verif_lines # On vérifie chaque ligne
+    # verif lignes
+    if @cases[0].content != " " and [@cases[0].content, @cases[1].content, @cases[2].content] == [@cases[0].content, @cases[0].content, @cases[0].content]
+      colorize_win(@cases[0], @cases[1], @cases[2]) # Si c'est bon on color en vert les cases
+      return true
+    elsif @cases[3].content != " " and [@cases[3].content, @cases[4].content, @cases[5].content] == [@cases[3].content, @cases[3].content, @cases[3].content]
+      colorize_win(@cases[3], @cases[4], @cases[5])
+      return true
+    elsif @cases[6].content != " " and [@cases[6].content, @cases[7].content, @cases[8].content] == [@cases[6].content, @cases[6].content, @cases[6].content]
+      colorize_win(@cases[6], @cases[7], @cases[8])
+      return true
+    else
+      return false 
+    end
+  end
+
+  def verif_columns
+    if @cases[0].content != " " and [@cases[0].content, @cases[3].content, @cases[6].content] == [@cases[0].content, @cases[0].content, @cases[0].content]
+      colorize_win(@cases[0], @cases[3], @cases[6])
+      return true
+    elsif @cases[1].content != " " and [@cases[1].content, @cases[4].content, @cases[7].content] == [@cases[1].content, @cases[1].content, @cases[1].content]
+      colorize_win(@cases[1], @cases[4], @cases[7])
+      return true
+    elsif @cases[2].content != " " and [@cases[2].content, @cases[5].content, @cases[8].content] == [@cases[2].content, @cases[2].content, @cases[2].content]
+      colorize_win(@cases[2], @cases[5], @cases[8])
+      return true
+    else
+      return false 
+    end
+
+  end
+
+  def verif_diagos
+    if @cases[0].content != " " and [@cases[0].content, @cases[4].content, @cases[8].content] == [@cases[0].content, @cases[0].content, @cases[0].content]
+      colorize_win(@cases[0], @cases[4], @cases[8])
+      return true
+    elsif @cases[2].content != " " and [@cases[2].content, @cases[4].content, @cases[6].content] == [@cases[2].content, @cases[2].content, @cases[2].content]
+      colorize_win(@cases[2], @cases[4], @cases[6])
+      return true
+    else
+      return false 
+    end
+  end
+
+  def colorize_win(cells1, cells2, cells3) # Color les cases en vert
+    cells1.change_color
+    cells2.change_color
+    cells3.change_color
   end
 end
--- lib/app/game.rb
+++ lib/app/game.rb
@@ -1,13 +1,25 @@
+# Game : cette classe s'occupe de gérer toute la partie. À l'initialisation elle crée 2 instances de Player, et 1 instance de Board.
+# Variables d'instance : tu peux lui rattacher les 2 players (sous forme d'un array), le Board, et des informations annexes comme 
+# "quel joueur doit jouer ?", "quel est le statut de la partie (en cours ? player1 a gagné ? etc.)", etc.
+# Méthodes : Game permet de jouer un tour (demande au joueur ce qu'il veut faire et rempli la case), s'occupe de finir la partie 
+# si un joueur a gagné et propose aux joueurs de faire une nouvelle partie ensuite.
+
 
 class Game
-  #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
-
+  # #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet
+  #  Player s'il gagne), le Board et un array contenant les 2 joueurs.
+  attr_accessor :current_player, :status
+end
   def initialize
     #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un current_player
+     players_array = [player1 = Player.new('Jo'), player2 = Player.new('Carl')]
+
+
   end
 
   def turn
-    #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
+    #TO DO : méthode faisant appel aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, 
+    #demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
   end
 
   def new_round
@@ -22,3 +34,53 @@ end
 
 
 
+
+
+
+CARSANO:
+
+class Game
+  attr_reader :board, :active_player, :j1, :j2
+  def initialize(j1,j2, active_player=j1)
+    # initialize une nouvelle partie
+    @board = Board.new # On crée un nouveau board
+    @cases_availables = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3", "QUITTER LA PARTIE"] # Les cases de choix de jeu
+    @prompt = TTY::Prompt.new
+    @j1 = j1
+    @j2 = j2
+    @active_player = active_player
+  end
+  def verify_endgame
+    # On va vérifier si 3 pions sont alignés ou si le jeu est complet, ou si le jeu est quitté en cours
+    if @board.verif_alignement_points or @board.nb_coups_joues == 9 or @board.aborted 
+      true
+    else
+      false
+    end
+  end
+
+  def place_value # On place la value
+    choice = @prompt.select("#{@active_player.name} (#{@active_player.symbol}), à vous de jouer : ", @cases_availables, cycle: true) 
+    @cases_availables.delete(choice)
+    @board.change_value_case(transform_choice(choice), @active_player.symbol)
+    change_active_player
+    choice = @prompt.select("#{@active_player.name} (#{@active_player.symbol}), à vous de jouer : ", @cases_availables, cycle: true) # On choisit parmi la liste des choix disponibles
+    @cases_availables.delete(choice) # On supprime de la liste le choix de l'utilisateur
+    @board.change_value_case(transform_choice(choice), @active_player.symbol) # On applique au boardcase correspondant la bonne valeur
+    change_active_player # On change de joueur
+  end
+
+  def transform_choice(choice)
+@@ -43,7 +43,7 @@ def transform_choice(choice)
+    end
+  end
+
+  def change_active_player
+  def change_active_player # Changement de joueur
+    if @active_player == @j1
+      @active_player = @j2
+    else
+      @active_player = @j1
+    end
+  end
+end
